47daa531508d4047574f07d377bd5899062e2217
Joao Ferreira <jferreira.ba@gmail.com>
2020-01-04 08:22:48 -0300

    refactor(app.js): modificado a versão do app

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send('Hello World! - v2'));
+app.get("/", (req, res) => res.send('Hello World! - v1.0.2'));
 
 app.listen(port, () => console.log("Server is running at port", port));
